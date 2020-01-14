0dcd5b4c2fba6c31491e9e9271279a691cc990e5
agnieszka-krawczenko <krawczenkoagnieszka@gmail.com>
2020-01-14 17:13:52 +0100

    Boy jumps, background moves

--- Game/script.js
+++ Game/script.js
@@ -16,11 +16,13 @@ const fast = 'KeyF';
 const playerWidth = parseInt(window.getComputedStyle(player).width);
 const playerHeight = parseInt(window.getComputedStyle(player).height);
 
-let playerSpeedX = 10;
-let playerSpeedY = 30;
+let playerSpeedX = 0;
+let playerSpeedY = 0;
+let playerMaxJump = 600;
 
 let playerPositionX = parseInt(window.getComputedStyle(player).left);
 let playerPositionY = parseInt(window.getComputedStyle(player).top);
+let playerStartPosition = playerPositionY;
 
 /*** Player Animation ***/
 
@@ -28,7 +30,6 @@ window.addEventListener('keydown', event => {
     console.log('event: ', event.code);
    
     if (event.code === run) {
-        
         document.getElementById('player-movement').className = 'player-movement player-run';
         if (playerPositionX + playerWidth + playerSpeedX <= worldWidth) {
             playerPositionX += playerSpeedX;
@@ -49,42 +50,51 @@ window.addEventListener('keydown', event => {
         }, 10);
     }*/
 
-    if (event.code === walk) {
-        document.getElementById('player-movement').className = 'player-movement player-walk';
-        player.style.transform = 'scaleX(-1)';
-        if (playerPositionX >= playerSpeedX) {
-            playerPositionX -= playerSpeedX;
-            player.style.left = `${playerPositionX}px`;   
-        }else{
-            player.style.left = `${playerPositionX}px`;
-        } 
-    }
+    // if (event.code === walk) {
+    //     document.getElementById('player-movement').className = 'player-movement player-walk';
+    //     player.style.transform = 'scaleX(-1)';
+    //     if (playerPositionX >= playerSpeedX) {
+    //         playerPositionX -= playerSpeedX;
+    //         player.style.left = `${playerPositionX}px`;   
+    //     }else{
+    //         player.style.left = `${playerPositionX}px`;
+    //     } 
+    // }
     if (event.code === jump) {
         document.getElementById('player-movement').className = 'player-movement player-jump';
-        if (playerPositionY >= playerSpeedY) {
-            playerPositionY -= playerSpeedY;
-            player.style.top = `${playerPositionY}px`;
-        }else{
-            player.style.top = `${playerPositionY}px`;
-        }
+            //player.style.top = `${playerMaxJump}px`;
+
+            playerPositionY = playerMaxJump;
+            player.style.top = `${playerPositionY}px`
+
+        // if (playerPositionY >= playerSpeedY) {
+        //     playerPositionY -= playerSpeedY;
+        //     player.style.top = `${playerPositionY}px`;
+        // }else{
+        //     player.style.top = `${playerPositionY}px`;
+        // }
     }
-    if (event.code === down) {
-        document.getElementById('player-movement').className = 'player-movement player-down';
-        if (playerPositionY + playerHeight + playerSpeedY <= worldHeight) {
-            playerPositionY += playerSpeedY;
-            player.style.top = `${playerPositionY}px`;
-        }else{
-            player.style.top = `${playerPositionY}px`;
-        }
-    }
-    if (event.code === kick) {
-        document.getElementById('player-movement').className = 'player-movement player-jump';
-    }
+    // if (event.code === down) {
+    //     document.getElementById('player-movement').className = 'player-movement player-down';
+    //     if (playerPositionY + playerHeight + playerSpeedY <= worldHeight) {
+    //         playerPositionY += playerSpeedY;
+    //         player.style.top = `${playerPositionY}px`;
+    //     }else{
+    //         player.style.top = `${playerPositionY}px`;
+    //     }
+    // }
+    // if (event.code === kick) {
+    //     document.getElementById('player-movement').className = 'player-movement player-jump';
+    // }
 });
 
 window.addEventListener('keyup', event => {
+    
     if ((event.code === run) || (event.code === walk) || (event.code === jump) || (event.code === kick)) {
-        document.getElementById('player-movement').className = 'player-movement player-idle';
+        
+        player.style.top = `${playerStartPosition}px`
+        document.getElementById('player-movement').className = 'player-movement player-run';
+
     }
     if (event.code === walk) {
         player.style.transform = '';
