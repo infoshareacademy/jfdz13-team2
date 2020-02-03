cccc5a73e5decb174d3437f6a6fe951fd5f31a0f
agnieszka-krawczenko <krawczenkoagnieszka@gmail.com>
2020-02-03 19:31:18 +0100

    Adden changeDots function

--- script.js
+++ script.js
@@ -1,47 +1,54 @@
-const heroImages = ['url("photos/heroImage.jpeg")', 'url("photos/heroImage1.jpg")', 'url("photos/heroImage2.jpg")'];
+const heroImages = ['url("photos/slajd0.jpg")', 'url("photos/slajd1.jpg")', 'url("photos/slajd2.jpg")'];
 let activeHeroImage = 0;
-const imgHtml = document.querySelector('.mainHero');
-const imgHtml1024px = document.querySelector('.mainHero__container');
-const titleHtml = document.querySelectorAll('.mainHero__title');
-const firstDot = document.querySelector('.firstSlide');
-const secondDot = document.querySelector('.secondSlide');
-const thirdDot = document.querySelector('.thirdSlide');
-const dotArray = [firstDot, secondDot, thirdDot];
+const heroImgHtmlMobile = document.querySelector('.mainHero');
+const heroImgHtmlDesktop = document.querySelector('.mainHero__container');
+const heroTitleHtml = [...document.querySelectorAll('.mainHero__title')];
+const dotsArray = [...document.querySelectorAll('.mainHero__dot')];
+const time = 4000;
+
+const changeDot = () => {
+  const activeDot = dotsArray.findIndex(dot => dot.classList.contains('currentDot'));
+  dotsArray[activeDot].classList.remove('currentDot');
+  dotsArray[activeHeroImage].classList.add('currentDot');
+}
 
 const changeHeroImages = () => {
+
   activeHeroImage++;
 
-  if (activeHeroImage == heroImages.length) {
+  if (activeHeroImage === heroImages.length) {
     activeHeroImage = 0;
   }
 
-  imgHtml.style.backgroundImage = heroImages[activeHeroImage];
-  imgHtml1024px.style.backgroundImage = heroImages[activeHeroImage];
+  heroImgHtmlMobile.style.backgroundImage = heroImages[activeHeroImage];
+  heroImgHtmlDesktop.style.backgroundImage = heroImages[activeHeroImage];
 
-  if (activeHeroImage == 2) {
-    titleHtml[0].style.color = '#fe466a';
-    titleHtml[1].style.color = '#fe466a';
-  } else {
-    titleHtml[0].style.color = 'transparent';
-    titleHtml[1].style.color = 'transparent';
-  }
-}
+  if (activeHeroImage === heroImages.length - 1) {
+    heroTitleHtml.forEach(element => element.classList.add('titleBorder')); 
+  };
+  
+  if (activeHeroImage !== heroImages.length - 1) {
+    heroTitleHtml.forEach(element => element.classList.remove('titleBorder')); 
+  } 
 
-setInterval(changeHeroImages, 4000);
+  changeDot();
+};
 
-firstDot.addEventListener('click', () => {
-  imgHtml.style.backgroundImage = heroImages[0];
-  imgHtml1024px.style.backgroundImage = heroImages[0];
-})
+setInterval(changeHeroImages, time);
 
-secondDot.addEventListener('click', () => {
-  imgHtml.style.backgroundImage = heroImages[1];
-  imgHtml1024px.style.backgroundImage = heroImages[1];
-})
+// dotArray[0].addEventListener('click', () => {
+//   imgHtml.style.backgroundImage = heroImages[0];
+//   imgHtml1024px.style.backgroundImage = heroImages[0];
+// })
 
-thirdDot.addEventListener('click', () => {
-  imgHtml.style.backgroundImage = heroImages[2];
-  imgHtml1024px.style.backgroundImage = heroImages[2];
-})
+// dotArray[1].addEventListener('click', () => {
+//   imgHtml.style.backgroundImage = heroImages[1];
+//   imgHtml1024px.style.backgroundImage = heroImages[1];
+// })
+
+// dotArray[2].addEventListener('click', () => {
+//   imgHtml.style.backgroundImage = heroImages[2];
+//   imgHtml1024px.style.backgroundImage = heroImages[2];
+// })
 
 
